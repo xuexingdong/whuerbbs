37a20142684cdae7d7fde2be15763a462da9b092
xuexingdong <306947352@qq.com>
2019-05-15 01:36:44 +0800

    完成交易区域和校区的数据

--- backend/src/main/java/cn/whuerbbs/backend/controller/v1/ConstantControllerV1.java
+++ backend/src/main/java/cn/whuerbbs/backend/controller/v1/ConstantControllerV1.java
@@ -2,9 +2,12 @@ package cn.whuerbbs.backend.controller.v1;
 
 import cn.whuerbbs.backend.common.AnonymousNameConstants;
 import cn.whuerbbs.backend.common.SchoolConstants;
+import cn.whuerbbs.backend.enumeration.Campus;
+import cn.whuerbbs.backend.enumeration.TradeCategory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,5 +27,23 @@ public class ConstantControllerV1 {
     @GetMapping("anonymous_names")
     public List<String> getAnonymousNames() {
         return AnonymousNameConstants.ANONYMOUS_NAME_LIST;
+    }
+
+    @GetMapping("trade_categories")
+    public Map<TradeCategory, String> getTradeCategories() {
+        var result = new HashMap<TradeCategory, String>();
+        for (TradeCategory tradeCategory : TradeCategory.values()) {
+            result.put(tradeCategory, tradeCategory.getTitle());
+        }
+        return result;
+    }
+
+    @GetMapping("campuses")
+    public Map<Campus, String> getCampuses() {
+        var result = new HashMap<Campus, String>();
+        for (Campus campus : Campus.values()) {
+            result.put(campus, campus.getTitle());
+        }
+        return result;
     }
 }
--- backend/src/main/java/cn/whuerbbs/backend/global/GlobalResponseBodyAdvice.java
+++ backend/src/main/java/cn/whuerbbs/backend/global/GlobalResponseBodyAdvice.java
@@ -9,7 +9,6 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-import java.util.Map;
 import java.util.Objects;
 
 @RestControllerAdvice
@@ -29,9 +28,6 @@ public class GlobalResponseBodyAdvice implements ResponseBodyAdvice<Object> {
             return Resp.data(((Page) body).getContent());
         }
         if (body instanceof String) {
-            return body;
-        }
-        if (body instanceof Map) {
             return body;
         }
         return Resp.data(body);
