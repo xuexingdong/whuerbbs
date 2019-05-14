49cf1827174094f732c4cc7aa5469b2762762382
xuexingdong <306947352@qq.com>
2019-05-15 01:01:39 +0800

    去除UserVO无用字段

--- backend/src/main/java/cn/whuerbbs/backend/vo/UserVO.java
+++ backend/src/main/java/cn/whuerbbs/backend/vo/UserVO.java
@@ -9,9 +9,6 @@ public class UserVO {
     private String nickname;
     private String avatarUrl;
     private Gender gender;
-    private String country;
-    private String province;
-    private String city;
     private String school;
     private String grade;
     private String diploma;
@@ -51,30 +48,6 @@ public class UserVO {
 
     public void setGender(Gender gender) {
         this.gender = gender;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getProvince() {
-        return province;
-    }
-
-    public void setProvince(String province) {
-        this.province = province;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
     }
 
     public String getSchool() {
